88ae9a251ff73eb30e55e85120ba31724a40ef9e
Robert Ramirez <rrami007@gmail.com>
2018-01-16 08:08:45 +0000

    revised

--- app.js
+++ app.js
@@ -1,12 +1,48 @@
-var express = require("express"),
-    app     = express();
+var express  = require("express"),
+    app      = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose");
     
-app.set("view engine", "ejs");
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/todo",{useMongoClient: true});    
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
 
+var tasklist = [
+        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
+        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
+        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"}
+];
+    
 app.get("/", function(req, res){
     res.render("home");
 });
+
+app.get("/tasklist", function(req, res){
+    res.render("tasklist",{tasklist:tasklist});
+});
+
+app.post("/tasklist", function(req, res){
+    // get data from form and add to tasklist array
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image}
+    tasklist.push(newCampground);
+    //redirect back to tasklist page
+    res.redirect("/tasklist");
+});
+
+app.get("/tasklist/new", function(req, res){
+   res.render("new.ejs"); 
+});    
+
     
     app.listen(process.env.PORT, process.env.IP, function(){
    console.log("The Server Has Started!");
