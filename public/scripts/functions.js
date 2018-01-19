7d96d1a5033d7b29d5bdff2e3fc6a8270d907c05
Robert Ramirez <rrami007@gmail.com>
2018-01-19 05:18:06 +0000

    update homepage

---
+++ public/scripts/functions.js
@@ -0,0 +1,20 @@
+(function ($) {
+
+	$('#countdown_dashboard').countDown({
+
+	        targetOffset: {
+	            'day':      0,
+	            'month':    0,
+	            'year':     1,
+	            'hour':     0,
+	            'min':      0,
+	            'sec':      0
+
+	        },
+
+	        
+	    });
+				
+
+})(jQuery);
+
